7d4dda795ed2391d0d377d0993f0ab7e02f89bb4
juniorpinhodev <juniorpinhodev@gmail.com>
2024-09-02 15:05:20 -0300

    feat: 🎸 Cria Cadastrando usuários enviando a permissao

---
+++ models/index.js
@@ -0,0 +1,35 @@
+const { Sequelize } = require('sequelize');
+const path = require('path');
+const fs = require('fs');
+
+// Carregar variáveis de ambiente
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  logging: false,
+});
+
+// Configuração para carregar todos os modelos da pasta models
+const models = {};
+const files = fs.readdirSync(__dirname);
+
+files.forEach((file) => {
+  if (file !== 'index.js') {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    models[model.name] = model;
+  }
+});
+
+// Estabelecer associações entre os modelos se necessário
+Object.keys(models).forEach((modelName) => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+module.exports = models;
---
+++ models/user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    permissao: {
+      type: DataTypes.STRING,
+      allowNull: true, 
+    },
+  }, {});
+
+
+  return User;
+};
